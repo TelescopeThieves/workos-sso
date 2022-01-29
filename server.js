9bcdc9d950eca23711399640f1657204d234bcca
Mario De Los Santos <mario.jdls@gmail.com>
2022-01-29 14:00:06 -0500

    imported path dependency

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require("cors");
 const apiRoutes = require("./routes/apiRoutes");
 const app = express();
+const path = require("path")
 require("dotenv").config();
 
 app.use(cors({ origin: "*" }));
